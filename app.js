173c65a20b168140ca71a5f59f1171c0fd0ef174
“Vince <vincenzo.reo@passportinc.com>
2024-04-18 10:01:45 -0400

    Working with npm packge: yargs and implementing basic note taking functionality

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const command = process.argv[2];
+yargs.version('1.1.0');
 
-if (command === 'add') {
-  console.log('Adding note.');
-} else if (command === 'remove') {
-  console.log('Removing note.');
-}
+// add, remove, list, read notes
+yargs.command({
+  command: 'add',
+  describe: 'Add a note',
+  handler: function () {
+    console.log('Adding a new note...');
+  }
+});
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing note...');
+  }
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler: function () {
+    console.log('Listing all notes...');
+  }
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'Read notes',
+  handler: function () {
+    console.log('Reading notes...');
+  }
+});
+
+console.log(yargs.argv);
